e0e8b49762c1552a63d6f1bdb9b977f3e967e57c
Aline <aline_lima@outlook.com>
2024-03-04 14:28:15 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,14 +1,28 @@
 def study_schedule(permanence_period, target_time):
-    if len(permanence_period) == 0 or target_time is None or target_time < 0:
+    if target_time is None:
         return None
 
+    count = 0
     for period in permanence_period:
-        if period[0] > period[1]:
+        start_time, end_time = period
+        if start_time is None or end_time is None:
+            return None
+        if not isinstance(start_time, int) or not isinstance(end_time, int):
             return None
 
-    present_students = set()
+    count = 0
     for period in permanence_period:
-        if period[0] <= target_time <= period[1]:
-            present_students.add(period[0])
+        start_time, end_time = period
+        if start_time <= target_time <= end_time:
+            count += 1
 
-            return len(present_students)
+    return count
+
+
+permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+target_times = [5, 4, 3, 2, 1]
+
+for target_time in target_times:
+    print(
+        f"target_time = {target_time}: saída: {study_schedule(permanence_period, target_time)}"
+    )
