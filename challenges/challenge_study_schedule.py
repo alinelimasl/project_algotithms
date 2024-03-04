a2b2a81e014cce63a59627968b2884ea78d6ad3f
Aline <aline_lima@outlook.com>
2024-03-04 14:05:07 -0300

    Inicia projeto

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(permanence_period) == 0 or target_time is None or target_time < 0:
+        return None
+
+    for period in permanence_period:
+        if period[0] > period[1]:
+            return None
+
+    present_students = set()
+    for period in permanence_period:
+        if period[0] <= target_time <= period[1]:
+            present_students.add(period[0])
+
+            return len(present_students)
