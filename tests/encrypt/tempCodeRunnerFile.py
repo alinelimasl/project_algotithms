e614be558af4bcea4acc6211c88069523411c6cd
Aline <aline_lima@outlook.com>
2024-03-04 15:06:08 -0300

    Requisito 2

--- tests/encrypt/tempCodeRunnerFile.py
+++ tests/encrypt/tempCodeRunnerFile.py
@@ -3,13 +3,9 @@ import pytest
 
 
 def test_encrypt_message():
-
-    assert encrypt_message("hello world", 3) == "leh_dlrow ol"
-    assert encrypt_message("hello world!", 0) == "!dlrow olleh"
+    assert encrypt_message("hello world", 3) == "olleh_dlrow ol"
+    assert encrypt_message("hello world!", 0) == "dlrow olleh!"
     with pytest.raises(TypeError):
         encrypt_message(123, 2)
     with pytest.raises(TypeError):
         encrypt_message("hello world!", "invalid_key")
-
-    assert encrypt_message("hello world", 2) == "dlrow oll_eh"
-    
