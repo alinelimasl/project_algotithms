be7ee6481a480aed6ebd7b18397c1f9c04677237
Aline <aline_lima@outlook.com>
2024-03-04 14:47:29 -0300

    Altera requisito 1 - menor complexidade)

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -5,24 +5,15 @@ def study_schedule(permanence_period, target_time):
     count = 0
     for period in permanence_period:
         start_time, end_time = period
-        if start_time is None or end_time is None:
-            return None
-        if not isinstance(start_time, int) or not isinstance(end_time, int):
+        if (
+            start_time is None
+            or end_time is None
+            or not isinstance(start_time, int)
+            or not isinstance(end_time, int)
+        ):
             return None
 
-    count = 0
-    for period in permanence_period:
-        start_time, end_time = period
         if start_time <= target_time <= end_time:
             count += 1
 
     return count
-
-
-permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
-target_times = [5, 4, 3, 2, 1]
-
-for target_time in target_times:
-    print(
-        f"target_time = {target_time}: saída: {study_schedule(permanence_period, target_time)}"
-    )
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,11 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("hello world", 3) == "olleh_dlrow ol"
+    assert encrypt_message("hello world!", 0) == "dlrow olleh"
+    with pytest.raises(TypeError):
+        encrypt_message(123, 2)
+    with pytest.raises(TypeError):
+        encrypt_message("hello world!", "invalid_key")
